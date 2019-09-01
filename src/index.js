cfb3a8d2a1fca7c30b6882a9d8ff500198f25a9c
Aseem <aseem2301@gmail.com>
2019-09-01 23:21:18 +0530

    added basic react timer implementation

--- src/index.js
+++ src/index.js
@@ -1,22 +1,51 @@
-import React, { Component } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 
-import styles from './styles.css'
+const ASCENDING = 'asc'
+const DESCENDING = 'desc'
 
-export default class ExampleComponent extends Component {
-  static propTypes = {
-    text: PropTypes.string
-  }
+const ReactTimer = ({
+  children,
+  start = 0,
+  end = 10,
+  delta = 1,
+  interval = 1000,
+  progression = ASCENDING,
+  onComplete = () => { },
+  onTick = () => { }
+}) => {
+  const [value, setValue] = useState(start)
 
-  render() {
-    const {
-      text
-    } = this.props
+  const computeResult = () => progression === DESCENDING ? (value - delta) : (value + delta)
 
-    return (
-      <div className={styles.test}>
-        Example Component: {text}
-      </div>
-    )
-  }
+  const evaluateCondition = () => progression === DESCENDING ? (value <= end) : (value >= end)
+
+  useEffect(() => {
+    const timer = window.setInterval(() => {
+      if (evaluateCondition()) {
+        window.clearInterval(timer)
+        onComplete(value)
+      } else {
+        const curr = computeResult()
+        setValue(curr)
+        onTick(curr)
+      }
+    }, interval)
+    return () => { window.clearInterval(timer) }
+  }, [value])
+
+  return children(value)
 }
+
+ReactTimer.propTypes = {
+  children: PropTypes.func.isRequired,
+  start: PropTypes.number.isRequired,
+  end: PropTypes.number.isRequired,
+  delta: PropTypes.number,
+  interval: PropTypes.number,
+  progression: PropTypes.oneOf([ASCENDING, DESCENDING]),
+  onComplete: PropTypes.func,
+  onTick: PropTypes.func
+}
+
+export default ReactTimer
